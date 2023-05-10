b23db05ad639cd25e744d37dac28844611a94384
DzianisPilipuk <dzianis.pilipuk@gmail.com>
2023-05-10 22:50:00 +0200

    Add winning pattern check

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gameboard = (() => {
-  const gameboardState = ["X", "X", "X", "X"];
+  const gameboardState = [];
   const gameboardCells = document.getElementById("gameboard").children;
   const addElement = (element, cell) => {
     gameboardState[cell] = element;
@@ -9,7 +9,51 @@ const gameboard = (() => {
       gameboardCells[i].textContent = gameboardState[i];
     }
   };
-  return { addElement, renderGameboard };
+  const getGameboardState = () => [...gameboardState];
+  return { addElement, renderGameboard, getGameboardState };
 })();
 
-gameboard.renderGameboard();
+const playerFactory = (team) => {
+  const playerTeam = team;
+  const makeMove = (cell) => {
+    gameboard.addElement(playerTeam, cell);
+  };
+  return { makeMove };
+};
+
+const player1 = playerFactory("X");
+
+const gameController = (() => {
+  let currentplayer = player1;
+  let gameIsFinished = false;
+  const checkGameIsFinished = () => {
+    const checkWinningPattern = (a, b, c) => {
+      const gameboardState = gameboard.getGameboardState();
+      if (
+        gameboardState[a] === gameboardState[b] &&
+        gameboardState[a] === gameboardState[c] &&
+        gameboardState[a] !== undefined
+      ) {
+        console.log("gameover");
+        gameIsFinished = true;
+      }
+    };
+    const winningPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winningPatterns.forEach((pattern) => checkWinningPattern(...pattern));
+  };
+  const playRound = (cell) => {
+    currentplayer.makeMove(cell);
+    gameboard.renderGameboard();
+    checkGameIsFinished();
+  };
+  return { playRound };
+})();
